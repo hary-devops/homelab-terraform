c028e13a787c9a989a72bbc77409f86540e5d4f4
harysetiawan <setiawan230896@gmail.com>
2025-02-15 16:52:33 +0800

    fix:

--- bootstraper/main.tf
+++ bootstraper/main.tf
@@ -1,6 +1,6 @@
 # Terraform stack using github.com as VCS
 resource "spacelift_stack" "authentik" {
-  administrative    = true
+  administrative    = false
   autodeploy        = true
   branch            = "master"
   description       = "Provision Authentik"
@@ -8,13 +8,13 @@ resource "spacelift_stack" "authentik" {
   project_root      = "environemnt/authentik"
   repository        = var.repository
   terraform_version = "1.3.0"
+  space_id          = "root"
 }
 
 
 resource "spacelift_environment_variable" "authentik-token" {
-  stack_id    = "${spacelift_stack.authentik.space_id}"
+  stack_id    = spacelift_stack.authentik.space_id
   name        = "AUTHENTIK_TOKEN"
-  value       = "XXXXXXX"
   write_only  = false
   description = "Authentik Token"
 }
