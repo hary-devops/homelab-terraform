5d49b636345cacd6ae3b1bfda16597e2ec0bb1ee
hary <hary@tripla.jp>
2025-11-22 12:11:10 +0800

    feat: add GitHub App Installation ID variable for Terraform Cloud VCS integration

--- main.tf
+++ main.tf
@@ -8,4 +8,10 @@ resource "tfe_workspace" "homelab_db" {
   organization      = "homelabhary"
   working_directory = "environment/db"
   project_id        = tfe_project.homelab_db.id
+
+  vcs_repo {
+    identifier     = "harysetiawan/homelab-terraform"
+    branch         = "main"
+    github_app_installation_id = var.github_app_installation_id
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "github_app_installation_id" {
+  description = "GitHub App Installation ID for Terraform Cloud VCS integration"
+  type        = string
+}
