e5c512b87963bbe445d8f2cc8e233e4314759a0a
hary <hary@tripla.jp>
2025-11-22 12:36:38 +0800

    fix: update .gitignore to include SSH keys and add missing newline; add null provider configuration in provider.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "github_app_installation_id" {
   description = "GitHub App Installation ID for Terraform Cloud VCS integration"
   type        = string
 }
+
+variable "docker_host_user" {
+  description = "SSH user for Docker host connection"
+  type        = string
+  default     = "harysetiawan"
+}
+
+variable "docker_host_ip" {
+  description = "IP address or hostname of the Docker host"
+  type        = string
+  default     = "192.168.18.28"
+}
