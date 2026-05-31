a2c85c2b54fad2e82230b78cc762827b4dc12c3e
harysetiawan <setiawan230896@gmail.com>
2026-05-31 15:33:04 +0800

    fix: add vcs repo

--- main.tf
+++ main.tf
@@ -11,4 +11,9 @@ resource "tfe_workspace" "homelab_aws_base" {
   name         = "homelab-aws-base"
   organization = data.tfe_organization.homelabhary.name
   project_id   = tfe_project.homelab_aws_base.id
+  vcs_repo {
+    github_app_installation_id = var.github_app_installation_id
+    identifier = "hary-devops/homelab-terraform"
+    branch = "master"
+  }
 }
